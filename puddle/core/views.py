bb08fc5fcffbbfbb077410a2153997ff60d26273
AshaJG <54907036+AshaJG@users.noreply.github.com>
2023-09-02 01:04:26 -0400

    Implementing Items in DB and on Website

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from item.models import Category , Item 
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    # putting items obasically on the frontpage 
+    items = Item.objects.filter(is_sold = False ) [0:6]
+    categories = Category.objects.all()
+    # add template in oder to use them 
+    return render(request, 'core/index.html' ,
+                  {
+                      'categories':categories,
+                      'items': items, 
+                  }
+                  )
 
 def contact(request):
     return render(request,'core/contact.html')
